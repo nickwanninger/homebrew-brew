ce528abe0c183973f6af39962bddb96e11de5ee8
Nick Wanninger <nickwanninger@gmail.com>
2018-05-17 01:59:44 -0400

    update dmp version

--- Formula/dmp.rb
+++ Formula/dmp.rb
@@ -5,7 +5,7 @@ class Dmp < Formula
   desc "Basic hex dump program"
   homepage "https://github.com/nickwanninger/dmp"
   url "https://github.com/nickwanninger/dmp/archive/master.zip"
-  version "0.0.1"
+  version "0.0.2"
   sha256 "8846594aafdb616486c9f439999f5f51b1d90df9340debd0e2234467e108fc2c"
   # depends_on "cmake" => :build
 
