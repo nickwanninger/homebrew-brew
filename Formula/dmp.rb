d9075a366d78404a1ce0653b630ddf725b14d633
Nick Wanninger <nickwanninger@gmail.com>
2018-05-17 01:50:09 -0400

    added dmp

---
+++ Formula/dmp.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Dmp < Formula
+  desc "Basic hex dump program"
+  homepage "https://github.com/nickwanninger/dmp"
+  url "https://github.com/nickwanninger/dmp/archive/master.zip"
+  version "0.0.1"
+  sha256 "8846594aafdb616486c9f439999f5f51b1d90df9340debd0e2234467e108fc2c"
+  # depends_on "cmake" => :build
+
+  def install
+    # system "cmake", ".", *std_cmake_args
+    system "make"
+    bin.install "dmp"
+  end
+
+  test do
+    system "false"
+  end
+end
