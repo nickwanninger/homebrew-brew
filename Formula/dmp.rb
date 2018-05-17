66e097afcab0b338c6a7dba7ccbd445d710c9da1
Nick Wanninger <nickwanninger@users.noreply.github.com>
2018-05-17 02:02:43 -0400

    Update dmp.rb

--- Formula/dmp.rb
+++ Formula/dmp.rb
@@ -1,11 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Dmp < Formula
   desc "Basic hex dump program"
   homepage "https://github.com/nickwanninger/dmp"
   url "https://github.com/nickwanninger/dmp/archive/master.zip"
-  version "0.0.2"
+  version "0.0.3"
   sha256 "8846594aafdb616486c9f439999f5f51b1d90df9340debd0e2234467e108fc2c"
   # depends_on "cmake" => :build
 
